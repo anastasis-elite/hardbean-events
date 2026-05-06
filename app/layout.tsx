6ddbb8979b1a70847223fd7dc8c8a382751e8598
anastasis-elite <lyndsbelle@gmail.com>
2026-05-06 13:22:08 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,24 +27,32 @@ export default function RootLayout({
 
         {children}
 
-        <footer className="siteFooter">
-          <div>
-            <h3>Hard Bean Event Center</h3>
-            <p>A warm, welcoming space for meaningful gatherings.</p>
-          </div>
+       <footer className="siteFooter">
+  <div className="footerBrand">
+    <h3>Hard Bean Event Center</h3>
+    <p>A warm, welcoming space for meaningful gatherings.</p>
+  </div>
 
-          <div className="footerContact">
-            <a href="tel:HER_PHONE_NUMBER">HER PHONE NUMBER</a>
-            <a href="mailto:HER_EMAIL_ADDRESS">HER EMAIL_ADDRESS</a>
-            <span>ADD CITY / ADDRESS HERE</span>
-          </div>
+  <div className="footerContact">
+    <a href="tel:HER_PHONE_NUMBER">HER PHONE NUMBER</a>
+    <a href="mailto:HER_EMAIL_ADDRESS">HER EMAIL ADDRESS</a>
+    <span>ADDRESS / CITY</span>
+  </div>
 
-          <div className="socialButtons">
-            <a className="socialButton facebook" href="FACEBOOK_URL">Facebook</a>
-            <a className="socialButton instagram" href="INSTAGRAM_URL">Instagram</a>
-            <a className="socialButton tiktok" href="TIKTOK_URL">TikTok</a>
-          </div>
-        </footer>
+  <div className="socialButtons">
+    <a className="socialButton facebook" href="FACEBOOK_URL">
+      Facebook
+    </a>
+
+    <a className="socialButton instagram" href="INSTAGRAM_URL">
+      Instagram
+    </a>
+
+    <a className="socialButton tiktok" href="TIKTOK_URL">
+      TikTok
+    </a>
+  </div>
+</footer>
       </body>
     </html>
   );
