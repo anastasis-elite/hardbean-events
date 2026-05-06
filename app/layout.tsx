e894daa17c792e63114d7496065bedd0a8343c4d
anastasis-elite <lyndsbelle@gmail.com>
2026-05-06 14:09:05 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,18 +41,13 @@ export default function RootLayout({
   </div>
 
   <div className="socialButtons">
-    <a className="socialButton facebook" href="facebook.com/hardbeanlumberton">
+    <a className="socialButton facebook" href="https://www.facebook.com/hardbeanlumberton">
       Facebook
     </a>
 
     <a className="socialButton instagram" href="INSTAGRAM_URL">
       Instagram
     </a>
-
-    <a className="socialButton tiktok" href="TIKTOK_URL">
-      TikTok
-    </a>
-  </div>
 </footer>
       </body>
     </html>
